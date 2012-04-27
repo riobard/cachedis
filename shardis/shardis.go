2dbe50b0be82d42a3dadb92ad60f16adb09a8293
Riobard <me@riobard.com>
2012-04-27 23:44:10 +0800

    Bugfix: get keys of map[string][]byte

--- shardis/shardis.go
+++ shardis/shardis.go
@@ -26,7 +26,7 @@ func keys(m map[string][]byte) []string {
     keys := make([]string, len(m))
     i := 0
     for k := range m {
-        keys[0] = k
+        keys[i] = k
         i++
     }
     return keys
